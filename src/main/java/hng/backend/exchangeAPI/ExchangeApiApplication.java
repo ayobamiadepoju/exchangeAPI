947bcbdda46e12ebf11957fb5ae2ffa04e21dcc9
Ayobami <ayobamiadepoju263@gmail.com>
2025-10-27 11:33:56 +0100

    Update ExchangeApiApplication.java

--- src/main/java/hng/backend/exchangeAPI/ExchangeApiApplication.java
+++ src/main/java/hng/backend/exchangeAPI/ExchangeApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExchangeApiApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("java.awt.headless", "true");
 		SpringApplication.run(ExchangeApiApplication.class, args);
 	}
 
